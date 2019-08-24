a8f5a87747ef6055913ba7ec58dfb5b37be764ab
Austin T <austin.taranto@gmail.com>
2019-08-23 21:27:45 -0400

    template of server file

---
+++ server.js
@@ -0,0 +1,27 @@
+// pasreses html to find elements 
+const cheerio = require("cheerio");
+// makes http request for the html page
+const axios = require("axios");
+
+// console.log()
+
+//make request with axios from website. html page is passed as callback's third argument 
+axios.get("webstie").then(function(response) {
+    //load html into cheerio and save as variable
+    // '$' becomes shorthand for cheerio commands
+    const $ = cheerio.load(response.data);
+    // empty array to save data that will be scrape
+    let results = [];
+    // with cheerio, find each p-tag (paragraph) with title of class
+    $("p.title").each(function(i, element) {
+        //save text of element in a "title" variable 
+        let title = $(element).children().attr("href");
+
+        //save results in object that will be pushed into results array
+        results.push({
+            title: title,
+            link: link
+        });
+    });
+    console.log(results);
+});
