26d84aa2ac2a92b187c13e904fa195c679fd9c56
Austin T <austin.taranto@gmail.com>
2019-08-24 21:41:23 -0400

    start server code for if it doesnt start with heroku

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// Start the server
+// app.listen(process.env.PORT || 3000, function () {
+//     console.log("App running on port " + PORT + "!");
+
 // pasreses html to find elements 
 const cheerio = require("cheerio");
 // makes http request for the html page
@@ -8,7 +12,7 @@ const mongojs = require("mongojs");
 
 const app = express();
 
-const databaseUrl = "scrapper";
+const databaseUrl = "news";
 const collections = ["scrappedData"];
 
 // hook mongojs config to db variable
@@ -66,26 +70,26 @@ app.listen(3000, function() {
 // console.log()
 
 //make request with axios from website. html page is passed as callback's third argument 
-axios.get("webstie").then(function(response) {
-    //load html into cheerio and save as variable
-    // '$' becomes shorthand for cheerio commands
-    const $ = cheerio.load(response.data);
-    // empty array to save data that will be scrape
-    let results = [];
-    // with cheerio, find each p-tag (paragraph) with title of class
-    $("p.title").each(function(i, element) {
-        //save text of element in a "title" variable 
-        let title = $(element).children().attr("href");
+// axios.get("webstie").then(function(response) {
+//     //load html into cheerio and save as variable
+//     // '$' becomes shorthand for cheerio commands
+//     const $ = cheerio.load(response.data);
+//     // empty array to save data that will be scrape
+//     let results = [];
+//     // with cheerio, find each p-tag (paragraph) with title of class
+//     $("p.title").each(function(i, element) {
+//         //save text of element in a "title" variable 
+//         let title = $(element).children().attr("href");
 
-        // find parent/child tag and save href values as "link"
-        // let link = $(element).parent().attr("href");
-        // let link = $(element).children().attr("href");
+//         // find parent/child tag and save href values as "link"
+//         // let link = $(element).parent().attr("href");
+//         // let link = $(element).children().attr("href");
 
-        //save results in object that will be pushed into results array
-        results.push({
-            title: title,
-            link: link
-        });
-    });
-    console.log(results);
-});
+//         //save results in object that will be pushed into results array
+//         results.push({
+//             title: title,
+//             link: link
+//         });
+//     });
+//     console.log(results);
+// });
