b1cbeb5232d4bd2d9adbfa202964fb11ba62c3fc
Austin T <austin.taranto@gmail.com>
2019-09-21 17:12:03 -0400

    tweaked notes.js

--- models/notes.js
+++ models/notes.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 
 const schema = mongoose.schema;
 
-const notesSchema = new schema ({
+const noteSchema = new schema ({
     title: String,
     sum: String,
     url: String
+    articles: {
+        type: schema.types.id,
+        ref: "Articles"
+    }
 });
 
 const Notes = module.exports = mongoose.model("Note", noteSchema);
